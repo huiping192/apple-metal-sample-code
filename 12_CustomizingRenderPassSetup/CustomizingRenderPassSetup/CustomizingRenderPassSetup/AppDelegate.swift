30af710d930b716a1934907b8c674742f40b7b0b
huiping_guo <huiping.guo@dena.jp>
2021-09-16 10:19:21 +0900

    12 CustomizingRenderPassSetup

---
+++ 12_CustomizingRenderPassSetup/CustomizingRenderPassSetup/CustomizingRenderPassSetup/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CustomizingRenderPassSetup
+//
+//  Created by Huiping Guo on 2021/09/15.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+  
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
